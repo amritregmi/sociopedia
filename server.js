24888cc1c6e329dc1586bbb7171d1046836830d1
Amrit <crazyregmi@gmail.com>
2021-02-12 07:04:27 +1100

    Adding unhandeled rejecttion and sig term heroku

--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 const mongoose = require('mongoose')
 const dotenv = require('dotenv')
-dotenv.config()
 
+/**
+ * @LISTENING to uncaught exception first.
+ * if any bugs happens in system, will run this code and stop server
+ */
+process.on('uncaughtException', err => {
+    console.log(err.name, err.message);
+    //console.log(error);
+    console.log('UNCAUGHT EXCEPTION ! SHUTTING DOWN SERVER...');
+    process.exit(1)
+})
+
+dotenv.config()
 const app = require('./app')
 
 const DB = process.env.DB_LINK.replace(
@@ -23,5 +34,37 @@ const port = process.env.PORT || 3000
 // run the server
 const server = app.listen(port, () => {
     console.log(`social awareness running on https://127.0.0.1:${port}`);
+    if (process.env.NODE_ENV === 'production') {
+        console.log('IN PRODUCTION MODE');
+    } else {
+        console.log('DEV MODE');
+    }
 })
+/**
+ * @LISTENING to Unhandled Rejection first.
+ * if any bugs happens in system, will run this code and stop server
+ * @EXAMPLE mongo authentication failed is not handled 
+ */
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('UNHANDLED REJECTION! SHUTTING DOWN SERVER...');
+    server.close(() => {
+        process.exit(1)
+    })
+})
+/**
+ * @DESC We start our app every 24 hrs to keep in good health. 
+ * @HEROKU does this by sending sigterm signal to our node app 
+ * @THEN our app shut downs immediately. 
+ * @SO this shutdown can be very bad, leaves current state in dry
+ * SIG TERM WILL CAUSE OUR APPLICATION TO SHUT DOWN 
+ */
+process.on('SIGTERM', () => {
+    console.log('SIGTERM RECEIVED.. Shutting down gracefully');
+    server.close(() => {
+        console.log('Process terminated');
+    })
+})
+
+
 module.exports = server
